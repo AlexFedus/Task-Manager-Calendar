2bd7ddcb60024fba19e046f6ffc8442987b231d7
AlexFedus <83235408+AlexFedus@users.noreply.github.com>
2022-04-21 15:02:57 -0400

    Began Javascript Portion

--- calendar.js
+++ calendar.js
@@ -1,1 +1,35 @@
+window.onload = pageLoad;
 
+
+
+
+
+function pageLoad() {
+    loadDate();
+    
+}
+
+function loadDate() {
+    current = new Date();
+    curmonth = current.getMonth();
+    curYear = current.getFullYear();
+
+    var months = ["Janauary", "February", "March", "April", "May", "June", "July", "August", "Septemeber", "Ocotober", "November", "December"]
+
+    monthandyear = months[curmonth] +", " + curYear;
+  
+    document.getElementById("monthyear").innerHTML = monthandyear;
+    loadCells(curmonth, curYear);
+
+}
+
+function loadCells(currmonth, curYear){
+    daysinmonth = new Date(curYear, currmonth + 1, 0).getDate();
+    console.log(daysinmonth);
+    firstDay = new Date(curYear, currmonth, 1);
+    console.log(firstDay);
+
+  
+    
+
+}
