6cc84ff4b2020780a59772f90c870efb40045822
AlexFedus <83235408+AlexFedus@users.noreply.github.com>
2022-04-21 19:59:57 -0400

    Added calendar

--- calendar.js
+++ calendar.js
@@ -6,7 +6,9 @@ window.onload = pageLoad;
 
 function pageLoad() {
     loadDate();
-    
+    var taskbutton = document.getElementById("button");
+    taskbutton.onclick = addtasks;
+
 }
 
 function loadDate() {
@@ -16,20 +18,73 @@ function loadDate() {
 
     var months = ["Janauary", "February", "March", "April", "May", "June", "July", "August", "Septemeber", "Ocotober", "November", "December"]
 
-    monthandyear = months[curmonth] +", " + curYear;
-  
+    monthandyear = months[curmonth] + ", " + curYear;
+
     document.getElementById("monthyear").innerHTML = monthandyear;
     loadCells(curmonth, curYear);
 
 }
 
-function loadCells(currmonth, curYear){
+function loadCells(currmonth, curYear) {
+    today = new Date();
     daysinmonth = new Date(curYear, currmonth + 1, 0).getDate();
     console.log(daysinmonth);
-    firstDay = new Date(curYear, currmonth, 1);
+    //firstDay = new Date(curYear, currmonth, 1);
+    firstDay = (new Date(curYear, currmonth)).getDay();
     console.log(firstDay);
 
-  
-    
+
+    tbl = document.getElementById("calbody");
+    tbl.innerHTML = "";
+
+
+    date = 1;
+
+    for (i = 0; i < 6; i++) {
+        row = document.createElement("tr");
+
+        for (let j = 0; j < 7; j++) {
+            if (i === 0 & j < firstDay) {
+                cell = document.createElement("td");
+
+                cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            else if (date > daysinmonth) {
+                break;
+            }
+
+            else {
+                idname = currmonth + "-" + date + "-" + curYear;
+                cell = document.createElement("td");
+                cell.setAttribute("id", idname);
+                cellText = document.createTextNode(date);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                date++;
+            }
+
+
+        }
+
+        tbl.appendChild(row); // appending each row into calendar body.
+    }
 
 }
+
+function addtasks() {
+    if (document.getElementById("taskname").value != "") {
+        var name = document.getElementById("taskname").value;
+        console.log(name);
+
+        
+    }
+}
+
+
+
+
+
+
+
